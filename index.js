a4507e78e5d0cfa14a53fa1d8c37eaa3febfd351
GeekyRahulVk <64090368+GeekyRahulVk@users.noreply.github.com>
2020-05-06 15:23:37 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,63 @@
+const path = require('path');
+const expressEdge = require('express-edge');
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const Post = require('./database/models/Post');
+const app = new express();
+const fileUpload = require("express-fileupload");
+
+
+mongoose.connect('mongodb://localhost:27017/my-blog', {
+        useNewUrlParser: true
+    })
+    .then(() => 'You are now connected to Mongo!')
+    .catch(err => console.error('Something went wrong', err))
+
+
+app.use(fileUpload());
+app.use(express.static('public'));
+app.use(expressEdge.engine);
+app.set('views', __dirname + '/views');
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.get('/', async (req, res) => {
+    const posts = await Post.find({})
+    res.render('index', {
+        posts
+    })
+});
+
+app.get('/post/:id', async (req, res) => {
+    const post = await Post.findById(req.params.id)
+    res.render('post', {
+        post
+    })
+});
+
+app.get('/posts/new', (req, res) => {
+    res.render('create')
+});
+
+app.post("/posts/store", (req, res) => {
+    const {
+        image
+    } = req.files
+
+    image.mv(path.resolve(__dirname, 'public/posts', image.name), (error) => {
+        Post.create({
+            ...req.body,
+            image: `/posts/${image.name}`
+        }, (error, post) => {
+            res.redirect('/');
+        });
+    })
+});
+ 
+
+app.listen(4000, () => {
+    console.log('App listening on port 4000')
+});
